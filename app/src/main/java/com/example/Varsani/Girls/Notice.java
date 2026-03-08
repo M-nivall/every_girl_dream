2227d318e358950765936784d78b831f28735365
nival <nivahmichael@gmail.com>
2026-03-08 19:21:31 +0300

    updated notice

--- app/src/main/java/com/example/Varsani/Girls/Notice.java
+++ app/src/main/java/com/example/Varsani/Girls/Notice.java
@@ -20,21 +20,26 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.Varsani.Clients.Models.UserModel;
 import com.example.Varsani.R;
 import com.example.Varsani.ReportCases.Adapters.AdapterEmergencyReport;
 import com.example.Varsani.ReportCases.Models.EmergencyModel;
+import com.example.Varsani.utils.SessionHandler;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Notice extends AppCompatActivity {
 
@@ -43,6 +48,9 @@ public class Notice extends AppCompatActivity {
     private AdapterEmergencyReport adapterEmergencyReport;
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
+
+    private SessionHandler session;
+    private UserModel user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +63,9 @@ public class Notice extends AppCompatActivity {
 
         recyclerView=findViewById(R.id.recyclerView);
         progressBar=findViewById(R.id.progressBar);
+
+        session=new SessionHandler(getApplicationContext());
+        user=session.getUserDetails();
 
         list=new ArrayList<>();
         recyclerView.setLayoutManager( new LinearLayoutManager( getApplicationContext() ) );
@@ -146,7 +157,15 @@ public class Notice extends AppCompatActivity {
                 toast.show();
                 Log.e("ERROR E ", error.toString());
             }
-        });
+        }){
+            @Override
+            protected Map<String,String> getParams()throws AuthFailureError {
+                Map<String,String>params=new HashMap<>();
+                params.put("userID",user.getClientID());
+                Log.e("PARAMS","" +params);
+                return params;
+            }
+        };
 
         RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
